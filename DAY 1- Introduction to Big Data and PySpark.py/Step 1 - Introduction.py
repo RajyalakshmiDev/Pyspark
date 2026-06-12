4d26eba4e83acbb5011e11907dab1bd03edd8c2c
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-06-12 16:52:46 +0530

    Step 1 - Introduction.py

---
+++ DAY 1- Introduction to Big Data and PySpark.py/Step 1 - Introduction.py
@@ -0,0 +1,18 @@
+Big Data:
+-----------
+Big Data is a large volume of data that cannot be processed efficiently using traditional tools and methods.
+
+--- Explanation:
+Normally, We can store and analyze small amounts of data using Excel, sql or python.
+However companies like Amazon, netflix and google generates millions of records every day.
+Examples:
+-> Customer orders 
+-> Video views
+-> Online Transaction 
+-> Search history
+
+When the data becomes too large to handle using traditional tools. it is called Big data.
+
+  
+  
+
